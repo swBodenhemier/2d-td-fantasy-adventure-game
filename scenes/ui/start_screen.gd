99a5feb46cc2707a64dceb9238efcd85baba5c67
swBodenhemier <swb79@nau.edu>
2026-09-20 15:35:34 -0700

    Add audio to title screen

--- scenes/ui/start_screen.gd
+++ scenes/ui/start_screen.gd
@@ -1,22 +1,14 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_start_button_pressed() -> void:
+	$ButtonClickAudio.play()
 	get_tree().change_scene_to_file(
 		"res://scenes/levels/main_game_level.tscn"
 	)
 
 func _on_quit_button_pressed() -> void:
+	$ButtonClickAudio.play()
 	$QuitConfirmationDialog.popup_centered()
 
 func _on_quit_confirmation_dialog_confirmed() -> void:
